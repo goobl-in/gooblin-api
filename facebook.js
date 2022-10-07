df5e090fdce7b8f294a19256cdc74557dd029ddc
Artur Gurgul <artur.gurgul@gmail.com>
2022-10-07 20:24:22 +0200

    add facebook login strategy

---
+++ facebook.js
@@ -0,0 +1,58 @@
+
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook')
+
+const facebookAppId = "403390698484542"
+const facebookAppSecret = "1f60151cc92cfaf04d6358fa56380e40"
+
+function enableFacebook() {
+    passport.use(new FacebookStrategy({
+        clientID: facebookAppId,
+        clientSecret: facebookAppSecret,
+        callbackURL: 'https://goobl.in/'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+    /*    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+        'https://www.facebook.com',
+        profile.id
+        ], function(err, cred) {
+        if (err) { return cb(err); }
+        if (!cred) {
+            // The Facebook account has not logged in to this app before.  Create a
+            // new user record and link it to the Facebook account.
+            db.run('INSERT INTO users (name) VALUES (?)', [
+            profile.displayName
+            ], function(err) {
+            if (err) { return cb(err); }
+
+            var id = this.lastID;
+            db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+                id,
+                'https://www.facebook.com',
+                profile.id
+            ], function(err) {
+                if (err) { return cb(err); }
+                var user = {
+                id: id.toString(),
+                name: profile.displayName
+                };
+                return cb(null, user);
+            });
+            });
+        } else {
+            // The Facebook account has previously logged in to the app.  Get the
+            // user record linked to the Facebook account and log the user in.
+            db.get('SELECT * FROM users WHERE id = ?', [ cred.user_id ], function(err, user) {
+            if (err) { return cb(err); }
+            if (!user) { return cb(null, false); }
+            return cb(null, user);
+            });
+        }
+        };
+        */
+    }
+    ))
+    
+}
+
+module.exports = enableFacebook
